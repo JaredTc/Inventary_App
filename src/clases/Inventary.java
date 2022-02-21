64ee1ac77d32b2f7c4077cc4da42f7441b678aae
JaredTc <jareda_g@outlook.es>
2022-02-20 19:26:28 -0600

    starting interfaces

--- src/clases/Inventary.java
+++ src/clases/Inventary.java
@@ -1,10 +1,15 @@
 
 package clases;
 
+import interfacez.Login;
+import interfacez.SignUp;
+
 public class Inventary {
 
     public static void main(String[] args) {
-     
+       
+        Login lg = new Login();
+        lg.setVisible(true);
     }
     
 }
--- src/clases/Mysqlconexion.java
+++ src/clases/Mysqlconexion.java
@@ -1,6 +1,26 @@
 
 package clases;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
 public class Mysqlconexion {
-    
+    public static Connection conectar = null;
+    public static Statement sentencia;
+    public static ResultSet resultado;
+
+    public static Connection conexion() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conectar = DriverManager.getConnection("jdbc:mysql://localhost:3307/inventary", "root", "");
+            sentencia = conectar.createStatement();
+        } catch (Exception e) {
+        }
+
+        return conectar;
+    }
 }
